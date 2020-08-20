8c410c6853250777d12f3f9dbbca098c5e3784d1
Ken Dreyer <kdreyer@redhat.com>
2020-08-20 16:40:39 -0600

    handle private branches in guess_osdist()
    The next release of rdopkg after 1.2.0 will properly handle private-
    branches, but for now we need to wrap this method and do the regex
    substitution ourselves.

--- test_tar_changes.py
+++ test_tar_changes.py
@@ -2,6 +2,7 @@ import os
 import importlib.machinery
 import importlib.util
 import py.path
+import pytest
 
 loader = importlib.machinery.SourceFileLoader('tarchanges', 'tar-changes')
 spec = importlib.util.spec_from_loader(loader.name, loader)
@@ -23,3 +24,14 @@ def test_clear_old_changes_sources(tmpdir, monkeypatch):
     sources = tmpdir.join('sources')
     expected = '3a393d427d5b16c33cf24da91244cc7a  ceph-12.2.8.tar.gz\n'
     sources.read() == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('master', 'RDO'),
+    ('ceph-5.0-rhel-8', 'RHCEPH'),
+    ('private-kdreyer-ceph-5.0-rhel-8', 'RHCEPH'),
+])
+def test_guess_osdist(monkeypatch, test_input, expected):
+    monkeypatch.setattr(tarchanges.guess, 'current_branch', lambda: test_input)
+    result = tarchanges.guess_osdist()
+    assert result == expected
