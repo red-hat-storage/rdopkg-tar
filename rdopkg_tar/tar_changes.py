d955bd3cb55e15e33c2dce3beee954bcb9910d86
Rory Wirch <rwirch@redhat.com>
2022-03-07 15:33:50 -0500

    get gitlab user and write to commit header file

--- rdopkg_tar/tar_changes.py
+++ rdopkg_tar/tar_changes.py
@@ -8,6 +8,7 @@ from rdopkg.utils.cmd import run
 from rdopkg.utils.git import git
 from rdopkg import guess
 import rdopkg.actions.distgit.actions
+import os
 
 
 def diff_filenames(base, branch):
@@ -181,8 +182,18 @@ def main():
         clear_old_changes_sources()
         run(fedpkg, 'upload', tarball, direct=True)
 
-    # Commit everything to dist-git
-    rdopkg.actions.distgit.actions.commit_distgit_update(branch=branch,
+    # Check for the original commiter username in jenkins env variables. 
+    # If it exists then build a header file for the new commit to preserve the original commiter.
+    userName = os.environ.get('gitlabUserUsername')
+    if userName:
+        with open("commit_header.txt","w") as fp:
+            fp.write("Original GitLab commiter: " + userName)
+        rdopkg.actions.distgit.actions.commit_distgit_update(branch=branch,
+                                                         local_patches_branch=patches_branch,
+                                                         commit_header_file="commit_header.txt")
+    else:
+        # Commit everything to dist-git
+        rdopkg.actions.distgit.actions.commit_distgit_update(branch=branch,
                                                          local_patches_branch=patches_branch)
     # Show the final commit
     rdopkg.actions.distgit.actions.final_spec_diff(branch=branch)
